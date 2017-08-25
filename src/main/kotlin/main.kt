5b74928e19a3bde8d0eaa80a316c73ea93ec5d10
equal-l2 <eng.equall2@gmail.com>
2017-08-26 07:23:39 +0900

    Use bit flags

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,58 +5,91 @@ fun main(args:Array<String>){
   Sokoban.setStageInfo(1);
   while(true){
     Sokoban.printAllCells();
+    if(Sokoban.isCleared()) break;
     Sokoban.inputMoveInfo();
     Sokoban.movePlayer();
-    if(Sokoban.isCleared()) break;
   }
-  println("Congratulation!");
+  println("\nCongratulations!");
 }
 
 object Sokoban {
 
-  class Stage(val map: Array<Array<Int>>, var stageObjs: Array<StageObj>){
+  class Stage(var map: Array<Array<Int>>){
     class StageObj(var x:Int, var y:Int, val dispStr:String);
     val x:Int;
     val y:Int;
-    var player = stageObjs[0];
+    var playerX = 0;
+    var playerY = 0;
     init{
       x = map[0].size;
       y = map.size;
       for(a in map){
         if(a.size != x){
-          throw IllegalArgumentException(String.format("y differs. map:%d y:%d",map.size,y))
+          throw IllegalArgumentException(String.format("y differs. map:%d y:%d",map.size,y));
         }
       }
-      for(o in stageObjs){
-        if ( o.x < 0 || o.x >= x) throw IllegalArgumentException(String.format("Object is out of bound. map:%d obj:%d",x,o.x))
-        if ( o.y < 0 || o.y >= y) throw IllegalArgumentException(String.format("Object is out of bound. map:%d obj:%d",y,o.y))
-      }
-    }
 
-    fun findCrate(x: Int, y: Int): Int {
-      for(i in 1 until stageObjs.size){
-        if(stageObjs[i].x == x && stageObjs[i].y == y) return i;
+      var cntDest   = 0;
+      var cntPlayer = 0;
+      var cntCrate  = 0;
+      for (yidx in 0 until y){
+        for (xidx in 0 until x){
+          val obj = map[yidx][xidx];
+          if(obj and DEST   != 0){
+            cntDest++;
+          }
+          if(obj and PLAYER != 0){
+            playerX = xidx;
+            playerY = yidx;
+            cntPlayer++;
+          }
+          if(obj and CRATE  != 0){
+            cntCrate++;
+          }
+        }
       }
-      return -1;
+      if(cntDest < 1){
+        throw IllegalArgumentException(String.format("Dest should be 1 at least. dest:%d",cntDest));
+      }
+      if(cntPlayer != 1){
+        throw IllegalArgumentException(String.format("Player should be only 1. player:%d",cntPlayer));
+      }
+      if(cntCrate < 1){
+        throw IllegalArgumentException(String.format("Crate should be 1 at least. crate:%d",cntCrate));
+      }
+      if(cntDest != cntCrate){
+        throw IllegalArgumentException(String.format("Dests and crates don't match. dest:%d crate:%d",cntDest,cntCrate));
+      }
     }
     companion object{
-      fun getPlayerObj(x: Int, y: Int): StageObj = StageObj(x,y,ansi.BG_RED+ansi.FG_WHITE+"PL"+ansi.RESET);
-      fun getCrateObj(x: Int, y: Int): StageObj  = StageObj(x,y,ansi.BG_YELLOW+ansi.FG_WHITE+"CR"+ansi.RESET);
+      val EMPTY  = 0b0000;
+      val DEST   = 0b0001;
+      val PLAYER = 0b0010;
+      val CRATE  = 0b0100;
+      val WALL   = 0b1000;
     }
   }
 
-
   lateinit var stage:Stage;
+
   var maxLength:Int = 0;
-  var cellStr:Array<String> = arrayOf(
-      "  ",                         // empty
-      ansi.BG_BLUE+"++"+ansi.RESET, // wall
-      "DS"                          // destination
+
+  var cellStr:Array<Array<String>> = arrayOf(
+      //(back,fore,str)
+      arrayOf("",              "",            "  "), // empty
+      arrayOf(ansi.BG_GREEN,   "",            "DS"), // destination
+      arrayOf(ansi.BG_RED,     "",            "PL"), // player
+      arrayOf(ansi.BG_YELLOW,  "",            "PL"), // player on dest.
+      arrayOf(ansi.BG_BLUE,    "",            "CR"), // crate.
+      arrayOf(ansi.BG_CYAN,    "",            "CR"), // crate on dest.
+      arrayOf("","",""),
+      arrayOf("","",""),
+      arrayOf(ansi.BG_MAGENTA, "",            "++")  // wall
   );
 
-  var moveX: Int = 0;
-  var moveY: Int = 0;
-  var moveCrate: Int = 0;
+  var moveX = 0;
+  var moveY = 0;
+  var moveCrate = false;
 
   fun printAllCells() {
     ansi.clearScreen();
@@ -65,56 +98,49 @@ object Sokoban {
         printCell(x,y);
       }
     }
-    for (o in stage.stageObjs){
-      printObj(o);
-    }
   }
 
   fun setStageInfo(stageNum:Int) {
-    // stage objects
-    // 0 : empty
-    // 1 : wall
-    // 2 : destination
+    // stage objects are described as bit flags.
+    // 0b0000 (0): empty
+    // 0b0001 (1): destination
+    // 0b0010 (2): player
+    // 0b0011 (3): player on dest.
+    // 0b0100 (4): crate
+    // 0b0101 (5): crate on dest.
+    // 0b0110 (6): (not in use)
+    // 0b0111 (7): (not in use)
+    // 0b1000 (8): wall
     if (stageNum==1) {
       stage = Stage(
-        arrayOf(
-          arrayOf(1, 1, 1, 1, 1, 1),
-          arrayOf(1, 1, 1, 0, 0, 1),
-          arrayOf(1, 1, 1, 0, 0, 1),
-          arrayOf(1, 0, 0, 2, 0, 1),
-          arrayOf(1, 0, 0, 0, 0, 1),
-          arrayOf(1, 1, 1, 0, 2, 1),
-          arrayOf(1, 1, 1, 0, 0, 1),
-          arrayOf(1, 1, 1, 1, 1, 1)
-        ),
-        arrayOf(
-            Stage.getPlayerObj(3,4),
-            Stage.getCrateObj(2,3),
-            Stage.getCrateObj(3,5)
-        )
+          arrayOf(
+              arrayOf(8, 8, 8, 8, 8, 8),
+              arrayOf(8, 8, 8, 0, 0, 8),
+              arrayOf(8, 8, 8, 0, 0, 8),
+              arrayOf(8, 0, 4, 1, 0, 8),
+              arrayOf(8, 0, 0, 2, 0, 8),
+              arrayOf(8, 8, 8, 4, 1, 8),
+              arrayOf(8, 8, 8, 0, 0, 8),
+              arrayOf(8, 8, 8, 8, 8, 8)
+          )
       );
     }
   }
 
   fun isCleared(): Boolean {
-    for (y in 0 until stage.y){
-      for (x in 0 until stage.x){
-        if(stage.map[y][x] == 2){
-          if(stage.findCrate(x,y) == -1) return false;
-        }
+    for (yObj in stage.map){
+      for (xObj in yObj){
+        if(xObj == Stage.CRATE) return false;
       }
     }
     return true;
   }
 
+  fun getCellStr(kind:Int) = cellStr[kind].reduce{a,b -> a+b}+ansi.RESET;
+
   fun printCell(x:Int, y:Int) {
     setLocation(x,y);
-    print(cellStr[stage.map[y][x]]);
-  }
-
-  fun printObj(o: Stage.StageObj) {
-    setLocation(o.x,o.y);
-    print(o.dispStr);
+    print(getCellStr(stage.map[y][x]));
   }
 
   fun setLocation(x:Int, y:Int){
@@ -128,10 +154,10 @@ object Sokoban {
     ansi.locateCursor(1,15);
     print("WASD: ");
     while(!(
-      buf.equals("W") || buf.equals("w") ||
-      buf.equals("S") || buf.equals("s") ||
-      buf.equals("A") || buf.equals("a") ||
-      buf.equals("D") || buf.equals("d") )
+        buf.equals("W") || buf.equals("w") ||
+        buf.equals("S") || buf.equals("s") ||
+        buf.equals("A") || buf.equals("a") ||
+        buf.equals("D") || buf.equals("d") )
     ){
       buf = readLine()?:"";
     }
@@ -142,35 +168,43 @@ object Sokoban {
     else if (buf.equals("D") || buf.equals("d")) moveX =  1;
   }
 
-  fun canMove(o: Stage.StageObj, isPlayer: Boolean): Boolean {
-    val toX = o.x + moveX;
-    val toY = o.y + moveY;
-    if ( toX < 0 || toX >= stage.x) return false;
-    if ( toY < 0 || toY >= stage.y) return false;
-    if (stage.map[toY][toX]==1)     return false;
-    val crate = stage.findCrate(toX, toY)
-    if (crate != -1){
-      if(!isPlayer) return false;
+  fun canMove(x: Int, y: Int, objIsPlayer: Boolean): Boolean {
+    val toX = x + moveX;
+    val toY = y + moveY;
+    val toObj = stage.map[toY][toX];
+    if (
+        (toX < 0 || toX >= stage.x) || // out-of-bound
+        (toY < 0 || toY >= stage.y) || // out-of-bound
+        (toObj == Stage.WALL)          /* you are in rock */
+    ) return false;
+
+    if (toObj and Stage.CRATE != 0){
+      if(objIsPlayer){
+        moveCrate = true;
+        return canMove(toX,toY,false);
+      }
       else{
-        moveCrate = crate;
-        return canMove(stage.stageObjs[crate],false);
+        return false;
       }
     }
     return true;
   }
 
   fun movePlayer(){
-    if(canMove(stage.player,true)){
-      if(moveCrate != 0){
-        var crate = stage.stageObjs[moveCrate];
-        crate.x += moveX;
-        crate.y += moveY;
+    val toX = stage.playerX + moveX;
+    val toY = stage.playerY + moveY;
+    if(canMove(stage.playerX,stage.playerY,true)){
+      if(moveCrate){
+        stage.map[toY][toX] = stage.map[toY][toX] and Stage.CRATE.inv();
+        stage.map[toY+moveY][toX+moveX] = stage.map[toY+moveY][toX+moveX] or Stage.CRATE;
       }
-      stage.player.x += moveX;
-      stage.player.y += moveY;
+      stage.map[stage.playerY][stage.playerX] = stage.map[stage.playerY][stage.playerX] and Stage.PLAYER.inv();
+      stage.map[toY][toX] = stage.map[toY][toX] or Stage.PLAYER;
+      stage.playerX = toX;
+      stage.playerY = toY;
     }
     moveX = 0;
     moveY = 0;
-    moveCrate = 0;
+    moveCrate = false;
   }
 }
