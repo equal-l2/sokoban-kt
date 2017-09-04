3f5091b4f4e9f24a3c11a952ee318fb372d0eca9
equal-l2 <eng.equall2@gmail.com>
2017-09-04 21:13:23 +0900

    Implement stage rendering

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,42 +1,42 @@
 package main
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.graphics.GL30
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile
+import sokoban.Sokoban
+
 /* ktlint-disable no-multi-spaces */
 
-object CUIInterface {
-    var engine = sokoban.Sokoban()
+object GUIInterface : ApplicationAdapter() {
+    enum class Direction {
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT
+    }
+    val tilePixel = 16
+    lateinit var map: TiledMap
+    lateinit var renderer: OrthogonalTiledMapRenderer
+    lateinit var camera: OrthographicCamera
+    lateinit var tiles: Array<Array<TextureRegion>>
+    var engine = Sokoban()
     var stageNum = 1
-    var cellStr = arrayOf(
-            //(background,string)
-            arrayOf("",              "  "), // empty
-            arrayOf(ansi.BG_GREEN,   "DS"), // destination
-            arrayOf(ansi.BG_BLUE,    "PL"), // player
-            arrayOf(ansi.BG_CYAN,    "PL"), // player on dest.
-            arrayOf(ansi.BG_RED,     "CR"), // crate
-            arrayOf(ansi.BG_YELLOW,  "CR"), // crate on dest.
-            arrayOf("", ""),
-            arrayOf("", ""),
-            arrayOf(ansi.BG_MAGENTA, "++")  // wall
-    )
-
-    fun setLocation(x: Int, y: Int) = ansi.locateCursor( x * 2 + 3, y + 2 )
-    fun getCellStr(kind: Int) = cellStr[kind].reduce { x, y -> x+y } + ansi.RESET
-
-    fun printCell(x: Int, y: Int) {
-        setLocation(x, y)
-        print(getCellStr(engine.stage.map[y][x]))
-    }
-
-    fun printAllCells() {
-        ansi.clearScreen()
-        for (y in 0 until engine.stage.y) {
-            for (x in 0 until engine.stage.x) {
-                printCell(x, y)
-            }
-        }
-    }
+    var playerDirection = Direction.UP
 
     fun readStage(): Boolean {
         try {
             engine.readStage("data/map/$stageNum.map")
+            updateMap()
             return true
         } catch (e: java.io.FileNotFoundException) {
             return false
@@ -49,61 +49,92 @@ object CUIInterface {
         if (!readStage()) kotlin.system.exitProcess(0)
     }
 
-    fun selectStage() {
-        while (true) {
-            print("Select stage number (Ctrl+D to abort) : ")
-            val buf = readLine()
-            if (buf == null) {
-                println("EOF detected")
-                kotlin.system.exitProcess(0)
-            } else {
-                val num = buf.toIntOrNull()
-                if (num == null) {
-                    println("Supplied number is not valid")
-                } else {
-                    stageNum = num
-                    if (!readStage()) {
-                        println("map/$stageNum.map was not found")
-                    } else {
-                        break
+    fun updateMap() {
+        var layer = TiledMapTileLayer(engine.stage.x, engine.stage.y, tilePixel, tilePixel)
+        for (y in 0 until engine.stage.y) {
+            for (x in 0 until engine.stage.x) {
+                val cell = TiledMapTileLayer.Cell().setTile(StaticTiledMapTile(
+                        when (engine.stage.map[y][x]) {
+                            Sokoban.Stage.EMPTY
+                                -> tiles[0][4]
+                            Sokoban.Stage.DEST
+                                -> tiles[0][2]
+                            Sokoban.Stage.PLAYER,
+                            (Sokoban.Stage.DEST or Sokoban.Stage.PLAYER)
+                                -> when (playerDirection) {
+                                    Direction.UP    -> tiles[0][5]
+                                    Direction.RIGHT -> tiles[0][6]
+                                    Direction.DOWN  -> tiles[0][7]
+                                    Direction.LEFT  -> tiles[0][8]
+                                }
+                            Sokoban.Stage.CRATE
+                                -> tiles[0][0]
+                            (Sokoban.Stage.DEST or Sokoban.Stage.CRATE)
+                                -> tiles[0][1]
+                            Sokoban.Stage.WALL
+                                -> tiles[0][9]
+                            else
+                                -> tiles[0][3]
+                        }
+                ))
+                layer.setCell(x, engine.stage.y-1-y, cell)
+            }
+        }
+        map = TiledMap()
+        map.getLayers().add(layer)
+        renderer = OrthogonalTiledMapRenderer(map, 1/10f)
+    }
+
+    override fun create() {
+        camera = OrthographicCamera()
+        camera.setToOrtho(false, 20f, 20f)
+        camera.update()
+        tiles = TextureRegion.split(Texture(Gdx.files.internal("data/tile.png")), tilePixel, tilePixel)
+        Gdx.input.setInputProcessor(
+                object : InputAdapter () {
+                    override fun keyDown(keyCode: Int) : Boolean {
+                        when (keyCode) {
+                            Input.Keys.LEFT  -> {
+                                engine.moveX = -1
+                                playerDirection = Direction.LEFT
+                            }
+                            Input.Keys.RIGHT -> {
+                                engine.moveX = 1
+                                playerDirection = Direction.RIGHT
+                            }
+                            Input.Keys.UP    -> {
+                                engine.moveY = -1
+                                playerDirection = Direction.UP
+                            }
+                            Input.Keys.DOWN  -> {
+                                engine.moveY = 1
+                                playerDirection = Direction.DOWN
+                            }
+                            else -> return true
+                        }
+
+                        engine.movePlayer()
+                        updateMap()
+                        return true
                     }
                 }
-            }
-        }
+        )
+        readStage()
     }
 
-    fun getInputForMove() {
-        var buf = ""
-        ansi.locateCursor(1, 15)
-        print("WASD: ")
-        while (arrayOf("w", "a", "s", "d").none { x -> buf.equals(x, true) }) {
-            buf = readLine() ?: ""
-        }
-
-        if      (buf.equals("w", true)) engine.moveY = -1
-        else if (buf.equals("s", true)) engine.moveY =  1
-        else if (buf.equals("a", true)) engine.moveX = -1
-        else if (buf.equals("d", true)) engine.moveX =  1
-    }
-
-    fun run() {
-        selectStage()
-        while (true) {
-            while (true) {
-                printAllCells()
-                if (engine.isCleared()) break
-                getInputForMove()
-                engine.movePlayer()
-            }
-            println("\nCongratulations!")
-            nextStage()
-            println("Press Enter Key to proceed")
-            println("Press Ctrl+D to abort")
-            if (readLine() == null) kotlin.system.exitProcess(0)
-        }
+    override fun render() {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT)
+        camera.update()
+        renderer.setView(camera)
+        renderer.render()
     }
 }
 
 fun main(args: Array<String>) {
-    CUIInterface.run()
+    var config = Lwjgl3ApplicationConfiguration()
+    config.setTitle("Sokoban")
+    config.setWindowedMode(480, 480)
+    config.setResizable(false)
+    Lwjgl3Application(GUIInterface, config)
 }
